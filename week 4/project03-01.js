69d9192fc376bcf07a6d3602d31e824d58f484db
adrienne2024 <reitzadrienne@gmail.com>
2024-09-06 13:41:46 -0700

    Add files via upload

---
+++ week 4/project03-01.js
@@ -0,0 +1,51 @@
+/*    JavaScript 7th Edition
+      Chapter 3
+      Project 03-01
+
+      Application to calculate total order cost
+      Author: Adrienne Reitz
+      Date: September 5, 2024
+
+      Filename: project03-01.js
+*/
+
+
+
+
+
+ // Function to display a numeric value as a text string in the format $##.## 
+ function formatCurrency(value) {
+    return "$" + value.toFixed(2);
+ }
+
+ let menuItems = [];
+ menuItems[document.getElementsByClassName ("menuItems")] [0]
+
+ let allInputs = document.getElementsByClassName("menuItems");
+ for (let i = 0; i < allInputs.length; i++) {
+    allInputs[i].addEventListener("click", calcTotal);
+
+ }
+
+ //create function to calculate total  cost of customer order
+   
+   function calcTotal() {
+   let orderTotal = 0;
+
+   for(let i = 0; i < document.getElementsByClassName("menuItems").length; i++)
+   Number(checkbox.value)
+   let i = 0
+   if (checkbox === "checkbox") {
+    orderTotal + menuItems
+
+   let html = document.getElementById("billTotal").innerHTML = formatCurrency(orderTotal)
+   
+   }
+    
+}
+
+
+
+
+   
+
