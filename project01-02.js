900f5bd2272441de13fe11117853b56f785288ef
adrienne2024 <reitzadrienne@gmail.com>
2024-09-21 08:27:41 -0700

    Add files via upload

---
+++ project01-02.js
@@ -0,0 +1,19 @@
+/*  JavaScript 7th Edition
+    Chapter 1
+    Hands-On Project 1-2
+
+    Author: Adrienne Reitz
+    Date:   August 26, 2024
+
+    Filename: project01-02.js
+*/
+
+//define variables for service name and service speed
+
+let service1Name = Basic, service1Speed = 0;
+
+let service2Name = Express, service2Speed = 100;
+
+let service3Name = Extreme, service3Speed = 500;
+
+let service4Name = Ultimate, service4Speed = 1;
